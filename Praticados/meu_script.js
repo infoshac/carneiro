2022229eb0ec4b96a93f9d445b462581a9f4e4d6
Instalx <instalx@MacBook-Air-de-Barnaby.local>
2020-10-03 15:46:38 -0300

    Elemento Dom, Array e praticas

---
+++ Praticados/meu_script.js
@@ -0,0 +1,42 @@
+// alert('Olá, seja bem vindo ao curso!!!')
+/*
+    selecionar um elemento no DOM
+    atualizar o valor desse elemento com uma string
+*/
+
+document.getElementById("nome").value = 'Oi';
+
+//document.getElementById("nome").value = '02';
+
+
+
+/*
+Se M estiver abaixo de 16 : Baixo peso muito grave
+b) Se M estiver entre 16 e 16,99: Baixo peso grave
+c) Se M estiver entre 17 e 18,49: Baixo peso
+d) Se M estiver entre 18,50 e 24,99: Peso normal
+e) Se M estiver entre 25 e 29,99: Sobrepeso
+f) Se M estiver entre 30 e 34,99: Obesidade grau I
+g) Se M estiver entre 35 e 39,99: Obesidade grau II
+h) Se M for maior que 40: Obesidade grau III
+*/
+
+//montar função
+
+var serie= 'Friends';
+if(true){
+	var serie2= 'Game of thrones';
+	document.write(serie2);
+}
+	document.write(serie2);
+	document.write('</br>');
+
+function x(){
+	var serie3= 'The Walking Dead';
+	document.write(serie);
+	document.write(serie2);
+}
+
+x();
+	document.write('</br>');
+	document.write(serie3);
